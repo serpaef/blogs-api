a09d229005706dfe3ef21b09cc6913b3c7340179
Fernando Serpa <serpaef@outlook.com>
2022-02-10 17:32:50 -0300

    fix: Trata resposta da validação para caso haja algum problema serverside

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -12,14 +12,19 @@ const login = express.Router();
 const invalidFields = { message: 'Invalid Fields' };
 
 async function validateEmail(req, res, next) {
-  const { email } = req.body;
+  try {
+    const { email } = req.body;
 
-  if (!email) return res.status(400).json(invalidFields);
+    if (!email) return res.status(400).json(invalidFields);
 
-  const exists = await UserServices.verifyExistingEmail(email);
-  if (!exists) return res.status(400).json(invalidFields);
+    const exists = await UserServices.verifyExistingEmail(email);
+    if (!exists) return res.status(400).json(invalidFields);
 
-  next();
+    next();
+  } catch ({ message }) {
+    console.error(message);
+    return res.status(500).json({ message: 'Server error, try again in a few minutes.' });
+  }
 }
 
 login.post('/',
