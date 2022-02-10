b40ae603e0ad2ac52dd273f227d11ff03c3ffc07
Fernando Serpa <serpaef@outlook.com>
2022-02-10 13:17:12 -0300

    feat: implementa rota user e passa a usar variaveis de ambiente

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+require('dotenv').config();
+
+const { PORT } = process.env;
+
+const UserController = require('./controllers/UserController');
 
 const app = express();
 
-app.listen(3000, () => console.log('ouvindo porta 3000!'));
+app.use(express.json());
+
+app.use('/user', UserController);
+
+app.listen(PORT, () => console.log(`ouvindo porta ${PORT}!`));
 
 app.get('/', (request, response) => {
   response.send();
