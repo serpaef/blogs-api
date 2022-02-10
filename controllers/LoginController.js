3fffd9ba1dc8defbf72ba40e314cc324e644447c
Fernando Serpa <serpaef@outlook.com>
2022-02-10 18:00:03 -0300

    feat: refatora validateEmail e implementa validatePassword

--- controllers/LoginController.js
+++ controllers/LoginController.js
@@ -9,16 +9,19 @@ const { JWT_SECRET } = process.env;
 
 const login = express.Router();
 
-const invalidFields = { message: 'Invalid Fields' };
-
 async function validateEmail(req, res, next) {
   try {
     const { email } = req.body;
 
-    if (!email) return res.status(400).json(invalidFields);
+    if (email === undefined) return res.status(400).json({ message: '"email" is required' });
+    if (email === '') {
+      return res.status(400).json({ message: '"email" is not allowed to be empty' });
+    }
 
     const exists = await UserServices.verifyExistingEmail(email);
-    if (!exists) return res.status(400).json(invalidFields);
+    if (!exists) return res.status(400).json({ message: 'Invalid fields' });
+
+    req.user = exists;
 
     next();
   } catch ({ message }) {
@@ -27,9 +30,31 @@ async function validateEmail(req, res, next) {
   }
 }
 
-login.post('/',
+function validatePassword(req, res, next) {
+  try {
+    const { password } = req.body;
+
+    if (password === undefined) return res.status(400).json({ message: '"password" is required' });
+    if (password === '') {
+      return res.status(400).json({ message: '"password" is not allowed to be empty' });
+    }
+
+    const { user } = req;
+
+    if (password !== user.password) return res.status(400).json({ message: 'Invalid fields' });
+
+    next();
+  } catch ({ message }) {
+    console.log(message);
+    return res.status(500).json({ message: 'Server error, try again in a few minutes.' });
+  }
+}
+
+login.post(
+  '/',
   validateEmail,
   validatePassword,
-  doLogin);
+  // doLogin
+);
 
 module.exports = login;
