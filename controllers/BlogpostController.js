df2283fa9b53a29a49ca34635e8343490d7da695
Fernando Serpa <serpaef@outlook.com>
2022-02-11 11:27:12 -0300

    feat: Implementa verificação de categorias da postagem

--- controllers/BlogpostController.js
+++ controllers/BlogpostController.js
@@ -6,6 +6,7 @@ const Auth = require('./Auth');
 
 const blogpost = express.Router();
 
+const CategoryServices = require('../services/CategoryServices');
 const BlogPostServices = require('../services/BlogpostServices');
 
 function verifyTitle(req, res, next) {
@@ -20,9 +21,30 @@ function verifyContent(req, res, next) {
   next();
 }
 
+async function verifyCategories(req, res, next) {
+  try {
+    const { categoryIds } = req.body;
+
+    if (!categoryIds || categoryIds.length < 1) {
+      return res.status(400).json({ message: '"categoryIds" is required' });
+    }
+
+    const categories = await CategoryServices.findAll({ where: { id: categoryIds } });
+    if (categories.length !== categoryIds.length) {
+      return res.status(400).json({ message: '"categoryIds" not found' });
+    }
+
+    next();
+  } catch ({ message }) {
+    console.error(message);
+    return res.status(500).json({ message: 'Server error, try again in a few minutes' });
+  }
+}
+
 blogpost.post('/',
   Auth,
   verifyTitle,
-  verifyContent);
+  verifyContent,
+  verifyCategories);
 
 module.exports = blogpost;
