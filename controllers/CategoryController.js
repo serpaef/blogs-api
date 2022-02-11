e7505d9223e4e2fe4ef82ba7ead4f647867dd9ac
Fernando Serpa <serpaef@outlook.com>
2022-02-11 09:45:37 -0300

    feat: Adiciona validação de nome para registro de categoria

--- controllers/CategoryController.js
+++ controllers/CategoryController.js
@@ -9,4 +9,14 @@ const { JWT_SECRET } = process.env;
 
 const category = express.Router();
 
+function validateName(req, res, next) {
+  const { name } = req.body;
+  if (!name) return res.status(400).json({ message: '"name" is required' });
+  next();
+}
+
+category.post('/',
+  validateName
+  Auth)
+
 module.exports = category;
